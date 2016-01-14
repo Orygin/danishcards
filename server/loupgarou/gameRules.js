9d475984377e901ce4cd29ba23dc97b51c01fe77
Louis Geuten <louisgeuten@gmail.com>
2016-01-14 19:04:18 +0100

    Loupgarou canvas updated

--- server/loupgarou/gameRules.js
+++ server/loupgarou/gameRules.js
@@ -1,4 +1,4 @@
-function danishGameRules(host){
+function loupGarouGameRules(host){
 	this.playerTurn = -1;
 	this.playingStack = [];
 	this.playingDeck = this.shuffleCards(staticCards.cards());
@@ -9,4 +9,12 @@ function danishGameRules(host){
 	});
 }
 
-danishGameRules.prototype.startGame = function () {
+loupGarouGameRules.prototype.startGame = function () {
+
+}
+loupGarouGameRules.prototype.setGameState = function (state) {
+	this.hostRoom.gameState = state;
+	this.hostRoom.sockets.emit('new game state', state);
+}
+loupGarouGameRules.prototype.checkEndGame = function () {}
+loupGarouGameRules.prototype.endGame = function () {}
